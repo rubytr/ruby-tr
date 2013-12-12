38bafb07e99e0ebf97a086e296b345dcdafae332
Serdar Dogruyol <dogruyolserdar@gmail.com>
2013-12-12 21:15:36 +0200

    Index working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 RubyTr::Application.routes.draw do
+  get "main/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
