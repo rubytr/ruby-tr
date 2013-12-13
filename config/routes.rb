217e83923a3fc2b3814f6160d4d7db4d41b82473
Serdar Dogruyol <dogruyolserdar@gmail.com>
2013-12-13 22:34:23 +0200

    Added people index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ RubyTr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'main#index', as: :root
+
+  get 'people' => 'person#index', as: :people
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
