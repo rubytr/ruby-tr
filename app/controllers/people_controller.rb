8704ec588d085d6152ec0aeb23c6dfe7c88a036a
İ. Emre Kutlu <emrekutlu@gmail.com>
2013-12-19 02:04:22 +0200

    fix params name

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,14 +10,13 @@ class PeopleController < ApplicationController
 
   def create
     @person = Person.new(person_params)
-    @person.save
-    redirect_to people_path
+    @person.save ? redirect_to(people_path) : render(:new)
   end
 
   private
 
   def person_params
-    params.require(:people).permit(:name, :email, :web, :github, :twitter, :humanizer_answer, :humanizer_question_id)
+    params.require(:person).permit(:name, :email, :web, :github, :twitter, :humanizer_answer, :humanizer_question_id)
   end
 
 end
