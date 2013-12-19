f69162ffcf7cc8e2a538053985e5e28323f8b449
Furkan Ayhan <furkanayhn@gmail.com>
2013-12-19 12:50:46 +0200

    email validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ActiveRecord::Base
   validates :name, :email, presence: true
   validates :name, :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
   before_save :get_gravatar_url
   include Humanizer
   require_human_on :create
