e8abed09333e67da572502a349bdeb08689d6ed8
Emre Ayduk <emre@nitro.io>
2013-12-23 20:46:22 +0200

    Remove unnecessary assets

--- app/assets/javascripts/googlegroups.js
+++
@@ -1,4 +0,0 @@
- document.getElementById("forum_embed").src =
-  "https://groups.google.com/forum/embed/?place=forum/ruby-tr" +
-  "&showsearch=true&showpopout=true&parenturl=" +
-  encodeURIComponent(window.location.href);
