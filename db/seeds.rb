3a44a950af1a7d31794e575560d67d929ab8c2ca
Ender Ahmet Yurt <enderyurt@gmail.com>
2013-12-24 23:35:28 +0200

    Generate companies from seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Company.delete_all
+
+companies = Company.create(
+    [
+      {id: 1, name: 'Enkuba', city: 'Istanbul', web: 'http://www.enkuba.com'},
+      {id: 2, name: 'Yeni Hayat', city: 'Istanbul', web: 'http://www.yh.com.tr'},
+      {id: 3, name: 'CloudArena', city: 'Istanbul', web: 'http://www.cloudarena.com'},
+      {id: 4, name: 'Promoqube', city: 'Istanbul', web: 'http://www.promoqube.com'}
+    ]
+  )
