3ff16a7bb21dfd7f2c33e69845b433df4fd69631
Ender Ahmet Yurt <enderyurt@gmail.com>
2017-09-12 17:09:16 +0300

    Remove callback line for Rails 5.1.x.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module RubyTr
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
 
     config.i18n.default_locale = :tr
 
