0ffa669e51eb1fe73f68fa6b266ea64ff7d36ccd
Ender Ahmet Yurt <enderyurt@gmail.com>
2017-09-22 14:07:43 +0300

    Fix tests.

--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -4,6 +4,5 @@ FactoryGirl.define do
   factory :announcement do
     title Faker::Hipster.sentence
     content Faker::Hipster.paragraph(20)
-    admin_user
   end
 end
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 RSpec.describe Announcement, type: :model do
   context '#create' do
     before do
-      @announcement = FactoryGirl.build(:announcement)
+      admin_user = FactoryGirl.build(:admin_user)
+      @announcement = FactoryGirl.build(:announcement, admin_user: admin_user)
     end
+
     it 'is invalid without title' do
       @announcement.title = nil
       expect(@announcement).not_to be_valid
