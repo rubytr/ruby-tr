2da6a23674c5308e429bf1a53d84f8c65f0a86da
Ender Ahmet Yurt <enderyurt@gmail.com>
2017-09-23 21:09:22 +0300

    Generate custom form.

--- app/admin/announcement.rb
+++ app/admin/announcement.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Announcement do
   permit_params :title, :content, :admin_user_id
+
+  form do |f|
+    f.inputs "Announcement Details" do
+      f.input :admin_user, as: :select, collection: AdminUser.all.map{|a| [a.email, a.id]}
+      f.input :title
+      f.input :content
+      f.submit
+    end
+  end
 end
