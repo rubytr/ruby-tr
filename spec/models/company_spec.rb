6295f7b2bd48e2511482cbe10a8dd66a80490589
Murat Bastas <muratbsts@gmail.com>
2017-10-01 21:51:07 +0300

    Company model spec rspec 3 syntax

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,10 +4,10 @@ describe Company do
 
   it { should belong_to(:user) }
 
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:sector) }
-  it { should validate_presence_of(:city) }
-  it { should validate_presence_of(:url) }
-  it { should validate_uniqueness_of(:title) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:sector) }
+  it { is_expected.to validate_presence_of(:city) }
+  it { is_expected.to validate_presence_of(:url) }
+  it { is_expected.to validate_uniqueness_of(:title) }
 
 end
