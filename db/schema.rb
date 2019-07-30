41bc6ca2c2eab3afe7e3ef99db7a600ebc24e6b3
coskuntekin <mail@coskuntekin.com>
2019-07-30 18:20:14 +0300

    Add ribbon

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2017_09_28_090543) do
 
-ActiveRecord::Schema.define(version: 20170928090543) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
