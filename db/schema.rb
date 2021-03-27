d266b603f1db2b5bbb12e295f4ada3d3468c0031
Eyüp Atiş <eyupatis@gmail.com>
2021-03-28 00:05:23 +0300

    fix small problems on db/schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,16 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2017_09_28_090543) do
 
-ActiveRecord::Schema.define(version: 20170928090543) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170928090543) do
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
@@ -96,4 +96,5 @@ ActiveRecord::Schema.define(version: 20170928090543) do
     t.string "last_sign_in_ip"
   end
 
+  add_foreign_key "announcements", "admin_users"
 end
