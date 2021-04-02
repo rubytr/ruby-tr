659879a6cec7f11c2d5865b4351895fd1336f109
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2021-04-02 22:53:45 +0300

    Revert "Feature/rails 6.1 (#155)" (#158)
    This reverts commit a19eadd837629a90cfaa6948d8edbcec05d0f3da.

--- config.ru
+++ config.ru
@@ -1,6 +1,4 @@
 # This file is used by Rack-based servers to start the application.
 
-require_relative 'config/environment'
-
+require ::File.expand_path('config/environment', __dir__)
 run Rails.application
-Rails.application.load_server
--- db/migrate/20210402184528_fix_active_admin_comments_index_name.rb
+++
@@ -1,9 +0,0 @@
-class FixActiveAdminCommentsIndexName < ActiveRecord::Migration[6.1]
-  def up
-    return unless index_name_exists?(:active_admin_comments, :index_active_admin_comments_on_author_type_and_author_id)
-
-    rename_index :active_admin_comments, :index_active_admin_comments_on_author_type_and_author_id, :index_active_admin_comments_on_author
-  end
-
-  def down;end
-end
