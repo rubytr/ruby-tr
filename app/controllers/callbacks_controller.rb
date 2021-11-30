1fa2143733b3a6fb9c143b8703cb07c67a85b5c6
nejdetkadir <nejdetkadir.550@gmail.com>
2021-11-30 20:14:11 +0300

    Fix omniauth with github

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,6 +2,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in @user
-    redirect_to request.referer
+    redirect_to companies_path
   end
 end
