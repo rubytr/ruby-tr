3758e812239f35b13de47a35e1292f0788634fea
nejdetkadir <nejdetkadir.550@gmail.com>
2021-12-07 02:36:34 +0300

    Show flash message after sign in with github

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,6 +2,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in @user
+    flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: "Github")
     redirect_to edit_user_registration_path
   end
 end
