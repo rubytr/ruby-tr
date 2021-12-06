5e9141a32aecfb7c17edd5b63ccf4d501f363f06
nejdetkadir <nejdetkadir.550@gmail.com>
2021-12-07 02:28:32 +0300

    Edit redirect path of omniauthentication

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,6 +2,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in @user
-    redirect_to companies_path
+    redirect_to edit_user_registration_path
   end
 end
